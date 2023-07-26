72bff829fd6ffa61adda2d245d38bf74825ab7ba
diegofrancisco <32806473+diegofrancisco@users.noreply.github.com>
2023-07-26 13:59:14 -0400

    Initial implementation

---
+++ slack-coverage-notification.py
@@ -0,0 +1,84 @@
+import slack
+import os
+import requests
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+class Emoji:
+    LIGHT = ":rotating_light:"
+    WARNING = ":warning:"
+    TADA = ":tada:"
+    FIRE = ":fire:"
+    ONE_HUNDRED = ":100:"
+
+    def __init__(self) -> None:
+        raise NotImplementedError
+
+def push_message_to_slack() -> None:
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path)
+
+    client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+    project_key = os.environ['PROJECT_KEY']
+    sonarcloud_token = os.environ['SONARCLOUD_TOKEN']
+
+    coverage = get_sonarcloud_coverage(project_key, sonarcloud_token)
+    if coverage is not None:
+        print(f"Coverage metric for project '{project_key}': {coverage}%")
+
+    client.chat_postMessage(channel='#slackbot-test', text=generate_status_emoji(coverage))
+
+def generate_status_emoji(code_coverage: str) -> str:
+    """return the status based on the code coverage percentage
+
+    :param code_coverage:               coverage including the "%" sign
+    :return:                            emoji value
+    """
+    status = ":sweat:"
+    code_coverage = int(code_coverage[:-1])
+
+    if code_coverage > 0 and code_coverage < 60:
+        status = Emoji.LIGHT
+    elif code_coverage >= 60 and code_coverage < 70:
+        status = Emoji.WARNING
+    elif code_coverage >= 70 and code_coverage < 80:
+        status = Emoji.TADA
+    elif code_coverage >= 80 and code_coverage < 100:
+        status = Emoji.FIRE
+    elif code_coverage >= 100:
+        status = Emoji.ONE_HUNDRED
+
+    return status
+
+def get_sonarcloud_coverage(project_key, sonarcloud_token):
+    base_url = "https://sonarcloud.io/api/measures/component"
+    
+    # API endpoint to fetch coverage metric
+    api_endpoint = f"{base_url}?component={project_key}&metricKeys=coverage"
+    
+    # Headers with authentication token
+    headers = {"Authorization": f"Basic {sonarcloud_token}"}
+    
+    try:
+        # Make the API request
+        response = requests.get(api_endpoint, headers=headers)
+        response_json = response.json()
+
+        # Check if the request was successful
+        if response.status_code == 200 and "component" in response_json:
+            # Extract the coverage metric from the response
+            coverage_metric = response_json["component"]["measures"][0]["value"]
+            return float(coverage_metric)
+        else:
+            print("Failed to retrieve coverage metric. Check your project key and authentication token.")
+            return None
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return None
+
+def main() -> None:
+    push_message_to_slack()
+
+if __name__ == "__main__":
+    main()
